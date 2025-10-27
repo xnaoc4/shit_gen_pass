0316d33041cf73d391d68ee6780dfc2b2164b67c
FiXtal <smirnovgleb541@gmail.com>
2025-10-27 21:18:43 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import secrets
+import string
+
+def gen_pass(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(secrets.choice(characters) for _ in range(length))
+    return password
+
+while True:
+    try:
+        user_length = int(input("Enter lentgh your password: "))
+        if user_length <= 0:
+            print(f"length {user_length} error")
+            continue
+        elif user_length <= 7:
+            print("error 0xFF")
+            continue
+        else:
+            print(f"Your password is: {gen_pass(user_length)}")
+            break
+    except ValueError:
+        print("error, write the int number")
