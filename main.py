cf9943a896e8f05a1823c4f7afbe6ec566d17223
FiXtal <smirnovgleb541@gmail.com>
2025-10-28 14:17:59 +0300

    shit commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import string
 
 def gen_pass(length):
     characters = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(secrets.choice(characters) for _ in range(length))
+    password = "".join(secrets.choice(characters) for _ in range(length))
     return password
 
 while True:
@@ -12,11 +12,11 @@ while True:
         if user_length <= 0:
             print(f"length {user_length} error")
             continue
-        elif user_length <= 7:
-            print("error 0xFF")
+        elif user_length <= 7 or user_length >= 128:
+            print("error. 0xFF")
             continue
         else:
             print(f"Your password is: {gen_pass(user_length)}")
             break
     except ValueError:
-        print("error, write the int number")
+        print("error. write the int number")
